bdc3fbd9a57cf7b2042dbbbcbcf45cea6514baed
Aron <emmanuel.aron@gmail.com>
2024-04-04 13:03:14 +0200

    Creation of day3.js and spec/day3.spec.js files, with a first test that needs to be implemented

---
+++ day3.js
@@ -0,0 +1,6 @@
+const isDigit = require('./common')
+
+function day3(){
+    console.log(isDigit(6));
+}
+day3()
---
+++ spec/day3.spec.js
@@ -0,0 +1,6 @@
+describe("function day3", function() {
+    it("Compute the sum of the numbers values , in a 2D-array of one element", function() {
+        expect([["467",".",".","114",".","."]]).toBe([ [581] ]);
+    });
+});
+

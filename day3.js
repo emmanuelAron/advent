c5ba3fff8000ceb9af64dbc9b3db61d8e153d4c5
Aron <emmanuel.aron@gmail.com>
2024-04-15 21:18:46 +0200

    adjacent of stars finished, and print his logs

--- day3.js
+++ day3.js
@@ -19,112 +19,139 @@ function isSpecial(char) {
  * If we dont consider the borders, we have 8 non-special character around a character */
 function adjacentOfStar(grid, row, col) {
     let result = [];
+    let adjObjs = {}
     let currentPos = grid[row][col]
     if (currentPos === '*') {
         let south = null;
         if (row + 1 < grid.length) {
             south = grid[row + 1][col];
-            
-            if (isDigit(south)){
-                console.log('south: ','line num: ',row + 1,'col num: ',col);
+
+            if (isDigit(south)) {
+                console.log('south: ', 'line num: ', row + 1, 'col num: ', col);
                 result.push(south)
-                
-            }     
+                if (adjObjs[south] === undefined) {
+                    adjObjs[south] = []
+                }
+                adjObjs[south].push([row + 1, col])
+            }
         }
         let north = null;
         if (row - 1 >= 0) {
             north = grid[row - 1][col];
-            
-            if (isDigit(north)){
-                console.log('north: ','line num: ',row - 1,'col num: ',col);
+
+            if (isDigit(north)) {
+                console.log('north: ', 'line num: ', row - 1, 'col num: ', col);
                 result.push(north);
-            }   
+                if (adjObjs[north] === undefined) {
+                    adjObjs[north] = []
+                }
+                adjObjs[north].push([row - 1, col])
+            }
         }
         let east = null;
         if (col + 1 < grid[row].length) {
             east = grid[row][col + 1];
-            
-            if (isDigit(east)){
-                console.log('east: ','line num: ',row,'col num: ',col+1);
+
+            if (isDigit(east)) {
+                console.log('east: ', 'line num: ', row, 'col num: ', col + 1);
                 result.push(east);
-               // console.log('E row: ',row,' col: ',col);
-            }  
+                if (adjObjs[east] === undefined) {
+                    adjObjs[east] = []
+                }
+                adjObjs[east].push([row, col + 1])
+            }
         }
         let west = null;
         if (col - 1 >= 0) {
             west = grid[row][col - 1];
-            
-            if (isDigit(west)){
-                console.log('west: ','line num: ',row,'col num: ',col-1);
+
+            if (isDigit(west)) {
+                console.log('west: ', 'line num: ', row, 'col num: ', col - 1);
                 result.push(west);
-            } 
+                if (adjObjs[west] === undefined) {
+                    adjObjs[west] = []
+                }
+                adjObjs[west].push([row, col - 1])
+            }
         }
         let southEast = null;
         if (row + 1 < grid.length && col + 1 < grid[row].length) {
             southEast = grid[row + 1][col + 1];
-            
-            if (isDigit(southEast)){
-                console.log('southEast: ','line num: ',row + 1,'col num: ',col + 1);
+
+            if (isDigit(southEast)) {
+                console.log('southEast: ', 'line num: ', row + 1, 'col num: ', col + 1);
                 result.push(southEast);
-            } 
+                if (adjObjs[southEast] === undefined) {
+                    adjObjs[southEast] = []
+                }
+                adjObjs[southEast].push([row + 1, col + 1])
+            }
         }
         let southWest = null;
         if (row + 1 < grid.length && col - 1 >= 0) {
             southWest = grid[row + 1][col - 1];
-            
-            if (isDigit(southWest)){
-                console.log('southWest: ','line num: ',row + 1,'col num: ',col - 1);
+
+            if (isDigit(southWest)) {
+                console.log('southWest: ', 'line num: ', row + 1, 'col num: ', col - 1);
                 result.push(southWest);
+                if (adjObjs[southWest] === undefined) {
+                    adjObjs[southWest] = []
+                }
+                adjObjs[southWest].push([row + 1, col - 1])
             }
         }
         let northEast = null;
         if (row - 1 >= 0 && col + 1 < grid[row].length) {
             northEast = grid[row - 1][col + 1];
-            
-            if (isDigit(northEast)){
-                console.log('NE row: ',row - 1,' col: ',col + 1);
+
+            if (isDigit(northEast)) {
+                console.log('NE row: ', row - 1, ' col: ', col + 1);
                 result.push(northEast);
-            }  
+                if (adjObjs[northEast] === undefined) {
+                    adjObjs[northEast] = []
+                }
+                adjObjs[northEast].push([row - 1, col + 1])
+            }
         }
         let northWest = null;
         if (row - 1 >= 0 && col - 1 >= 0) {
             northWest = grid[row - 1][col - 1];
-            
-            if (isDigit(northWest)){
-                console.log('NW row: ',row - 1,' col: ',col - 1);
+
+            if (isDigit(northWest)) {
+                console.log('NW row: ', row - 1, ' col: ', col - 1);
                 result.push(northWest);
-            }  
+                if (adjObjs[northWest] === undefined) {
+                    adjObjs[northWest] = []
+                }
+                adjObjs[northWest].push([row - 1, col - 1])
+            }
         }
     }
-    return result;
+    return { result, adjObjs };
 }
 
-adjCols = [-1,0,1] //indexes for adjacents rows
-adjRows = [-1,0,1] //indexes for adjacents columns
 
 adjacents = []
 
 readFileLines(file)
     .then((lines) => {
-        const grid = processFileAsGrid(lines) 
-       // console.log('grid: ',grid.length);
-        for(let lineNum = 0; lineNum< grid.length;lineNum++){
-            for(let colNum = 0; colNum< grid.length; colNum++){
+        const grid = processFileAsGrid(lines)
+        for (let lineNum = 0; lineNum < grid.length; lineNum++) {
+            for (let colNum = 0; colNum < grid.length; colNum++) {
                 let myChar = grid[lineNum][colNum]
-                //console.log(myChar);
-                if(myChar === '*'){
-                   // console.log(myChar, 'lineNum:',lineNum,' colNum: ',colNum);
-                    console.log(adjacentOfStar(grid, lineNum, colNum))
-                    // for(a_row in adjRows){
-                    //     for(b_col in adjRows){
-                    //         //we remove to coordinate of the star itself from the list.
-                    //         console.log('adjacents coordinates: line indexes:',lineNum +adjCols[a_row],'rows indexes: ',colNum+adjRows[b_col], 'a_row: ',a_row,' b_col: ',b_col);
-                            
-                    //     }
-                    // }
+                if (myChar === '*') {
+                    const { result, adjObjs } = adjacentOfStar(grid, lineNum, colNum)
+                    for (const key in adjObjs) {
+                        if (adjObjs[key].length !== 0) {
+                            console.log('adjacent number: ', key, 'his coordinates: ', adjObjs[key]);
+                        }
+
+                    }
                 }
             }
         }
-    }).catch((error)=>{
+    }).catch((error) => {
         console.error(`Error while reading the file : ${error}`)
     })
+
+
